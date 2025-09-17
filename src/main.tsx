74a07c9a65499ae63b411a4f4094be47b5aeb524
CeYn97 <Kfur4ik@gmail.com>
2025-09-17 17:28:02 +0300

    final

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-<style>
-@import url('https://fonts.googleapis.com/css2?family=ABeeZee:ital@0;1&display=swap');
-</style>
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
